ca0a5dacdf77edbeae44a8bcc22871502bec44b5
thym101 <tmaiaa@siue.edu>
2024-10-19 15:33:45 -0500

    battle scene

--- Jammm/Script/BaseEnemy.gd
+++ Jammm/Script/BaseEnemy.gd
@@ -2,6 +2,8 @@ extends Resource
 
 @export var name: String = "Enemy"
 @export var texture: Texture = null
+@export var health: int = 100
+@export var damage: int = 10
 @export var arm: int
 @export var leg: int
 @export var chest: int
--- Jammm/Script/Battle.gd
+++ Jammm/Script/Battle.gd
@@ -3,12 +3,14 @@ extends Control
 @export var enemy: Resource = null
 var current_player_health = 0
 var current_enemy_health = 0
-# Called when the node enters the scene tree for the first time.
+var is_enemy_attack = false
+
 func _ready():
-	set_health($Player/ProgressBar, Health.current_health, Health.max_health)
-	set_health($Monster/ProgressBar, enemy.health, enemy.health)
-	$Monster/Dragon/DragonPoke.texture = enemy.texture
+	set_health($Player/Panel2/ProgressBar, Health.current_health, Health.max_health)
+	set_health($Monster/Panel/ProgressBar, enemy.health, enemy.health)
+	#$Monster/Dragon/DragonPoke.texture = enemy.texture
 	#$Action.hide()
+	$Action/textbox.hide()
 	
 	current_player_health = Health.current_health
 	current_enemy_health = enemy.health
@@ -17,49 +19,98 @@ func _ready():
 func _process(delta):
 	pass
 
+func display_text(text):
+	$Action/textbox.show()
+	$Action/textbox.text = text
+	
 func set_health(progress_bar, health, max_health):
 	progress_bar.value = health
 	progress_bar.max_value = max_health
-
+#
 func enemy_turn():
 	current_player_health = max(0, current_player_health - enemy.damage)
-	set_health($Player/ProgressBar, current_player_health, Health.max_health)
+	set_health($Player/Panel2/ProgressBar, current_player_health, Health.max_health)
 	
 	$AnimationPlayer.play("player_damaged")
-
-func _on_arm_pressed():
-	current_enemy_health = max(0, current_enemy_health - Health.damage)
-	set_health($Monster/ProgressBar, current_enemy_health, enemy.health)
+	display_text("Player attacked")
 	
+	await get_tree().create_timer(1).timeout
+	close_textbox()
+	
+	await get_tree().create_timer(0.2).timeout
+	$Action/action_button.visible = true
+	
+func _on_arm_pressed():
+	$Action/action_button.visible = false
+	
+	display_text("Give it a nice hand cut")
+	
+	await get_tree().create_timer(0.7).timeout
+	current_enemy_health = max(0, current_enemy_health - Health.damage)
+	set_health($Monster/Panel/ProgressBar, current_enemy_health, enemy.health)
 	$AnimationPlayer.play("enemy_damage")
-
+	
+	#wait for 1 second and then close textbox
+	await get_tree().create_timer(1).timeout
+	close_textbox()
+	
+	await get_tree().create_timer(1).timeout
 	enemy_turn()
-
 
 func _on_leg_pressed():
-	current_enemy_health = max(0, current_enemy_health - Health.damage)
-	set_health($Monster/ProgressBar, current_enemy_health, enemy.health)
+	$Action/action_button.visible = false
 	
+	display_text("Leg has a cut")
+	
+	await get_tree().create_timer(0.7).timeout
+	current_enemy_health = max(0, current_enemy_health - Health.damage)
+	set_health($Monster/Panel/ProgressBar, current_enemy_health, enemy.health)
 	$AnimationPlayer.play("enemy_damage")
 
+	await get_tree().create_timer(1).timeout
+	close_textbox()
+	
+	await get_tree().create_timer(1).timeout
 	enemy_turn()
 	
-
-func _on_chest_pressed():
-	current_enemy_health = max(0, current_enemy_health - Health.damage)
-	set_health($Monster/ProgressBar, current_enemy_health, enemy.health)
 	
+func _on_chest_pressed():
+	$Action/action_button.visible = false
+	
+	#display text
+	display_text("Chest cut")
+	
+	#health bar decreasing
+	await get_tree().create_timer(0.7).timeout
+	current_enemy_health = max(0, current_enemy_health - Health.damage)
+	set_health($Monster/Panel/ProgressBar, current_enemy_health, enemy.health)
+	
+	#animation while being attacked
 	$AnimationPlayer.play("enemy_damage")
 
+	await get_tree().create_timer(1).timeout
+	close_textbox()
+	
+	await get_tree().create_timer(1).timeout
 	enemy_turn()
 	
 
 func _on_appendages_pressed():
-	current_enemy_health = max(0, current_enemy_health - Health.damage)
-	set_health($Monster/ProgressBar, current_enemy_health, enemy.health)
+	$Action/action_button.visible = false
 	
+	display_text("No more wing")
+	
+	await get_tree().create_timer(0.7).timeout
+	current_enemy_health = max(0, current_enemy_health - Health.damage)
+	set_health($Monster/Panel/ProgressBar, current_enemy_health, enemy.health)
 	$AnimationPlayer.play("enemy_damage")
-
+	
+	await get_tree().create_timer(1).timeout
+	close_textbox()
+	
+	await get_tree().create_timer(1).timeout
 	enemy_turn()
 	
+func close_textbox():
+	$Action/textbox.visible = false
 
